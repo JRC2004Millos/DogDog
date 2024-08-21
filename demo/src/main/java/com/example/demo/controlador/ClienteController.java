22917b55533ce60d5c1238866dfd9c72c174967f
Julian <julianrodriguezc@javeriana.edu.co>
2024-08-21 18:22:29 -0500

    Mascota relacionada a cliente

--- demo/src/main/java/com/example/demo/controlador/ClienteController.java
+++ demo/src/main/java/com/example/demo/controlador/ClienteController.java
@@ -1,7 +1,5 @@
 package com.example.demo.controlador;
 
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.entidad.Cliente;
-import com.example.demo.repositorio.MascotaRepository;
 import com.example.demo.servicio.ClienteService;
 
 @Controller
@@ -35,7 +32,6 @@ public class ClienteController {
 
         if (cliente != null) {
             model.addAttribute("cliente", service.findById(identificacion));
-            model.addAttribute("mascotas", new ArrayList<>(mascotaRepository.findByDuenoId(identificacion)));
         } else {
             throw new NotFoundException(identificacion);
         }
