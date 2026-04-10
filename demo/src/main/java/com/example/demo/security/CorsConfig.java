bf3faa05b5593997fa34cbf9884a3409f95e97b3
rinconjd <rinconjd@javeriana.edu.co>
2026-04-10 16:43:29 -0500

    cors erro

--- demo/src/main/java/com/example/demo/security/CorsConfig.java
+++ demo/src/main/java/com/example/demo/security/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedOrigin("http://localhost");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
